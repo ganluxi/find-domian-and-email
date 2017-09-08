28e066bd2e29de9c0424b751dae2bd1d6a1157f1
ganluxi <31759435+ganluxi@users.noreply.github.com>
2017-09-08 16:04:25 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+class config(object):
+    def get_sql_connect_info(self):
+        #conn_info={'host':'127.0.0.1','port':3306, 'user':'root','passwd':'root','db':'crawl','charset':'utf8'}
+        conn_info={'host':'172.93.38.53','port':3306, 'user':'root','passwd':'d$%DJH#87H76','db':'crawl','charset':'utf8'}
+        return conn_info
